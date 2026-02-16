3a90f59e36d4274c5e6603a105fb455737838930
Aastha Sharma K M <aastha08222006@gmail.com>
2026-02-16 19:00:43 +0530

    Initial commit:Created a hello_fork.c and setup gitignore

---
+++ hello_fork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() 
+{
+    pid_t id = fork(); // This is the "Magic" from L-1.10!
+
+    if (id < 0) {
+        printf("Fork Failed!\n");
+    } else if (id == 0) {
+        printf("Hello! I am the Child Process. My ID is %d\n", getpid());
+    } else {
+        printf("Hello! I am the Parent Process. My Child's ID is %d\n", id);
+    }
+
+    return 0;
+}
